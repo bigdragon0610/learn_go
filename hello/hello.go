2df45a3bcd501eafc7f140ca75f0569108bcc782
ryudai <bigdragon.06102001@gmail.com>
2023-03-30 00:41:47 +0900

    チュートリアルの修了

--- hello/hello.go
+++ hello/hello.go
@@ -1,9 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
 
-import "rsc.io/quote"
+	"example.com/greetings"
+)
 
 func main() {
-    fmt.Println(quote.Go())
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	messages, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, message := range messages {
+		fmt.Println(message)
+	}
 }
